b9276082c7b45848ddae4574f4a67bce292c74a4
Kapong <165357918+kaponguncle@users.noreply.github.com>
2024-06-16 21:35:13 +0700

    New Code -> Replace New One

---
+++ upgrade_main.py
@@ -0,0 +1,16 @@
+# รายการสินค้า
+productList = {
+    "prod1": {
+        "name": "Laptop",
+        "price": 34000
+    },
+    "prod2": {
+        "name": "Tablet",
+        "price": 19000
+    },
+    "prod3": {
+        "name": "Smartphone",
+        "price": 26000
+    }
+}
+
