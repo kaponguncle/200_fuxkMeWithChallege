c8eba3a802ec68eb52c393b9a7d7cbbfe5c4c0d4
Kapong <165357918+kaponguncle@users.noreply.github.com>
2024-06-16 22:59:11 +0700

    Add test file and upgrade_main.py

---
+++ test.py
@@ -0,0 +1,20 @@
+# รายการสินค้า
+productList = {
+    1: {
+        "name": "Laptop",
+        "price": 34000,
+        "amountCustomer": 0
+    },
+    2: {
+        "name": "Tablet",
+        "price": 19000,
+        "amountCustomer": 0
+    },
+    3: {
+        "name": "Smartphone",
+        "price": 26000,
+        "amountCustomer": 0
+    }
+}
+
+print(productList[1]["amountCustomer"])
--- upgrade_main.py
+++ upgrade_main.py
@@ -1,16 +1,50 @@
 # รายการสินค้า
 productList = {
-    "prod1": {
+    1: {
         "name": "Laptop",
-        "price": 34000
+        "price": 34000,
+        "amountCustomer": 0
     },
-    "prod2": {
+    2: {
         "name": "Tablet",
-        "price": 19000
+        "price": 19000,
+        "amountCustomer": 0
     },
-    "prod3": {
+    3: {
         "name": "Smartphone",
-        "price": 26000
+        "price": 26000,
+        "amountCustomer": 0
     }
 }
 
+# ส่วนลดต่าง ๆ
+discountCustomer = 0.05
+discountOver50k = 0.1
+
+while True:
+    try:
+        # แสดงรายการสินค้า
+        print("**************** CSAI Shop ****************")
+        for productName, productInfo in productList.items():
+            print(f"{productName} - {productInfo}")
+        print("*******************************************")
+
+        # รับค่าเพื่อเลือกสินค้า
+        selectItemInput = int(input("เลือกรายการสินค้า (1-3): "))
+        amountSelectItemInput = int(input("จำนวนสินค้าที่ต้องการ: "))
+
+        # ถ้า amountCustomer ไม่เท่ากับ 0 ให้เพิ่มข้อมูล
+        if productList[selectItemInput]["amountCustomer"] != 0:
+            productList[selectItemInput].update({"amountCustomer": productList[selectItemInput]["amountCustomer"]+amountSelectItemInput})
+        else:
+            productList[selectItemInput].update({"amountCustomer": amountSelectItemInput})
+    
+    except IndexError:
+        print("[ Error ] ไม่พบรายการที่คุณป้อนเข้ามาในระบบ, กรุณาลองอีกครั้ง")
+
+    except KeyboardInterrupt:
+        print(f"\n[ Info ] รายการที่ถูกสั่ง: {productInfo}")
+        exit()
+    
+    except Exception as error:
+        print(f"\n[ Error ] พบปัญหาในการทำงาน: {error}")
